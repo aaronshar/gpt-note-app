b144e0254c51a6fbf16a05adc9ec3350b649d802
Hae-Ji Park <positive235@gmail.com>
2024-04-24 10:10:45 -0700

    Users can see shrinked menu when they reduce their screen size. When they hover the navigation menu and sign in button, font color or background color will change.

--- frontend/pages/navigation/navbar/User.tsx
+++ frontend/pages/navigation/navbar/User.tsx
@@ -8,7 +8,7 @@
 
 const User = () => {
     return (
-      <button className="h-12 rounded-lg bg-white font-bold px-5">Sign In</button>
+      <button className="sign-in h-12 rounded-lg bg-white font-bold px-5">Sign In</button>
       
     );
   };
--- frontend/pages/navigation/navbar/index.tsx
+++ frontend/pages/navigation/navbar/index.tsx
@@ -6,59 +6,37 @@
   - How to create a responsive navigation bar (https://medium.com/@a.pirus/how-to-create-a-responsive-navigation-bar-in-next-js-13-e5540789a017)
 */
 
-import React, { useState } from "react";
+"use client";
+import React from "react";
 import Link from "next/link";
 import Logo from "./Logo";
 import User from "./User";
-import Sidebar from "../sidebar"
 
-const Navbar = ({ toggle }: { toggle: () => void }) => {
+const Navbar = () => {
   return (
     <>
       <div className="w-full h-20 navbar sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <Logo />
-            <button
-              type="button"
-              className="inline-flex items-center md:hidden"
-              onClick={toggle}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="40"
-                height="40"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="3" y1="12" x2="21" y2="12" />
-                <line x1="3" y1="6" x2="21" y2="6" />
-                <line x1="3" y1="18" x2="21" y2="18" />
-              </svg>
-            </button>
-            <Sidebar toggle={toggle}/>
-            <ul className="hidden md:flex gap-x-6 font-bold">
-              <li>
+            <ul className="md:flex gap-x-6 font-bold">
+              <li className="menu">
                 <Link href="/">
                   <p>Transcribe</p>
                 </Link>
               </li>
-              <li>
+              <li className="menu">
                 <Link href="/uploadPage">
                   <p>Upload</p>
                 </Link>
               </li>
-              <li>
+              <li className="menu">
                 <Link href="/myNotesPage">
                   <p>My Notes</p>
                 </Link>
               </li>
             </ul>
-            <div className="hidden md:block">
+            <div className="md:block">
               <User />
             </div>
           </div>
